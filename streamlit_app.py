81899bdbd0ccc6ddf014bb52b79fb848bc5d8167
Sean Naughton <sean-naughton@users.noreply.github.com>
2022-09-07 14:47:58 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.title('Sean doing some stuff')
 streamlit.header('Breakfast Faavorites')
@@ -18,4 +19,6 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
-streamlit.header('Sean showing this to Julie')
+streamlit.header('Fruityvice Fruit Advice!')
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
